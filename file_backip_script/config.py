51d50478773338b3c017e83c705b42fbb5bb6817
HIGGSTAR\jimmy.chen <jimmy.chen@higgstar.com>
2021-03-19 17:50:19 +0800

    rsync err

--- file_backip_script/config.py
+++ file_backip_script/config.py
@@ -1,2 +1,9 @@
+
+log_dir = "/var/log/backup"
+cloud_ip = "172.16.68.156"
 cloud_mount = "/data-disk/backup"
+cloud_user = "backupadmin"
+
+local_interface = "ens192"
+local_mount = "/data-disk/backup"
 backup_list = ["/root/test", "/root/test2/sync"]
--- file_backip_script/main_backup.py
+++ file_backip_script/main_backup.py
@@ -2,55 +2,87 @@
 # -*- coding: UTF-8 -*-
 import os
 import shutil
+import subprocess
 import time
 import datetime
-import pathlib
+import os.path  #for py2
+#import pathlib #for py3
 import sys
 import config
+import socket
+import fcntl
+import struct
 from logger import Logger
 
 today = str(datetime.date.today())
 logger = Logger("backup.log")
 
 
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+def creat_dir(filepath):
+    directory = os.path.dirname(filepath)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    else:
+        logger.log("INFO", + filepath + " dir is exist")
+
 
 def pre_backup():
     logger.log("INFO", "Starting Pre_backup...")
-    dirCheck = os.system("cd " + config.cloud_mount)
-    if dirCheck != 0:
-        createBackup = pathlib.Path(config.cloud_mount).mkdir(parents=True, exist_ok=True)
-        if createBackup == 0:
-            os.system("cd " + config.cloud_mount + " && mkdir " + str(today) )
-            logger.log("SUCCESS", " Pre_backup successfully created dir")
-        else:
-            logger.log("ERROR", " Pre_backup failed created dir")
-            sys.exit(1)
+    logger.log("INFO", "Pre_backup task1 checking log dir")
+
+    logDir = os.system("cd " + config.log_dir)
+    try:
+        if logDir != 0:
+            creat_dir(config.log_dir)
+    except:
+        logger.log("ERROR", "Pre_backup task1 An exception occurred")
+        sys.exit(1)
     else:
-        os.system("cd " + config.cloud_mount + " && mkdir " + str(today) )
-        logger.log("SUCCESS", " Pre_backup successfully created directory")
+        logger.log("SUCCESS", "Pre_backup task1 log dir is exist")
+
+    logger.log("INFO", "Pre_backup task2 checking daily dir")
+    dirCheck = os.system("cd " + config.local_mount + '/' + str(get_ip_address(config.local_interface)) )
+    try:
+        if dirCheck != 0:
+            creat_dir(config.local_mount + '/' + str(get_ip_address(config.local_interface)) + '/' + str(today) + '/') #最後要留/才會跟著建立dir
+            logger.log("SUCCESS", "Pre_backup task2 successfully")
+    except:
+        logger.log("ERROR", "Pre_backup task2 An exception occurred")
+        sys.exit(1)
+    else:
+        creat_dir(config.local_mount + '/' + str(get_ip_address(config.local_interface)) + '/' + str(today) + '/')
+        logger.log("SUCCESS", "Pre_backup task2 creat daily dir successfully")
 
 
 def backup():
-    logger.log("INFO", "Starting backup to " + str(config.cloud_mount + '/' + today) + " directories...")
-    if os.path.isdir(str(config.cloud_mount + '/' + today )):
+    logger.log("INFO", "Starting backup to " + str(local_backup_dir)  + " directories...")
+    if os.path.isdir(str(local_backup_dir)):
         for var in config.backup_list:
             try:
                 if os.path.isfile(var):#如果是檔案
                     logger.log("INFO", "Starting backup file " + var)
-                    status = os.system("cd " + config.cloud_mount + '/' + today + " && rsync -avhR " + var + ' .')
+                    status = os.system("cd " + str(local_backup_dir) + " && rsync -ahR " + var + ' .')
                     if status == 0:
                         logger.log("SUCCESS", "Backup file "+ var +" successfully")
                     else:
                         logger.log("ERROR", "Failed to backup "+ var)
                 elif os.path.isdir(var): #如果是資料夾
                     logger.log("INFO", "Starting backup dir " + var)
-                    status = os.system("cd " + config.cloud_mount + '/' + today + " && rsync -avhR " + var + ' .')
+                    status = os.system("cd " + str(local_backup_dir) + " && rsync -ahR " + var + ' .')
                     if status == 0:
                         logger.log("SUCCESS", "Backup dir "+ var +" successfully")
                     else:
                         logger.log("ERROR", "Failed to backup "+ var)
                 else:
-                        logger.log("ERROR", var + " not exist this dir or file backup failed.Please check it!")
+                        logger.log("ERROR", var + " not exist .Please check config.py file")
             except:
                 logger.log("ERROR", "An exception occurred")
                 sys.exit(1)
@@ -59,31 +91,30 @@ def backup():
         sys.exit(1)
 
 
-
 def rsyncBackups():
-    logger.log("INFO", "Starting rsync to remote backup dir...")
-    for file in os.listdir(config.cloud_mount):
-        if os.path.isfile(file):
-            os.remove(file)
-        elif os.path.isdir(file):
-            os.rmdir(file)
-        else:
-            logger.log("ERROR", file + " can't be deleted!")
-    logger.log("SUCCESS", " Successfully deleted old backup files")
+    logger.log("INFO", "Starting sync to remote dir...")
+    try:
+        #sendCheck = subprocess.call(["rsync", "-avh", str(local_backup_dir) + ' ' + config.cloud_user + '@' + config.cloud_ip + ':' + config.cloud_mount + '/' + str(get_ip_address(config.local_interface)) + '/'])
+        subprocess.call(["rsync", "-avh", str(local_backup_dir) + ' ' + config.cloud_user + '@' + config.cloud_ip + ':' + config.cloud_mount + '/' + str(get_ip_address(config.local_interface)) + '/'])
+    except OSError as e:
+        print(e.output)
+        logger.log("ERROR", "rsync to remote task An exception occurred")
+        sys.exit(1)
+    else:
+        logger.log("SUCCESS", "Successfully rsync to remote dir")
 
 
-
-if os.path.exists(config.cloud_mount + '/' + today):
-    shutil.rmtree(config.cloud_mount + '/' + today)
+init_check = (config.local_mount + '/' + str(get_ip_address(config.local_interface)) + '/' + today)
+local_backup_dir = str(config.local_mount + '/' + str(get_ip_address(config.local_interface)) + '/' + today)
+#try:
+if os.path.exists(init_check):
+    shutil.rmtree(config.local_mount + '/' + str(get_ip_address(config.local_interface)) + '/' + today)
     pre_backup()
     backup()
-    #rsyncBackups()
-elif os.path.exists(config.cloud_mount):
-    pre_backup()
-    backup()
-    #rsyncBackups()
+    rsyncBackups()
 else:
-    logger.log("ERROR", "This backup nothing to do please check backup directory")
-    logger.closeFile()
-    sys.exit(1)
-logger.closeFile()
+    pre_backup()
+    backup()
+    rsyncBackups()
+#except:
+#    logger.log("ERROR", "init_check An exception occurred")
